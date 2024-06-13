b719b41ddfd08a10119d476414233dc6b0c162d9
Fardeen S <fardeenclan@gmail.com>
2024-06-13 18:01:10 +0530

    added search

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,10 +1,13 @@
-import { UserButton } from "@clerk/nextjs";
-import React from "react";
+import Categories from "@/components/Categories";
+import SearchInput from "@/components/SearchInput";
+import prismadb from "@/lib/prismadb";
 
-const RootPage = () => {
+const RootPage = async () => {
+  const categories = await prismadb.category.findMany();
   return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
+    <div className="h-full p-4 space-y-2">
+      <SearchInput />
+      <Categories data={categories} />
     </div>
   );
 };
